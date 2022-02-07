5b9d8c739c896a5ed024d738e73bcbded35e036e
Steven Nguyen <stevenn38@hotmail.com>
2022-02-08 12:34:43 +1300

    Create and test styled Headings

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import React from "react";
 
 // Components
 import BackgroundStars from "components/BackgroundStars";
+import { H1, H2, H3, H4 } from "components/Headings";
 
 function App() {
-    return <BackgroundStars></BackgroundStars>;
+    return (
+        <BackgroundStars>
+            <H1>Hello World</H1>
+            <H2>Hello World</H2>
+            <H3>Hello World</H3>
+            <H4>Hello World</H4>
+        </BackgroundStars>
+    );
 }
 
 export default App;
