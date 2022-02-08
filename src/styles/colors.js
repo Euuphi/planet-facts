c6bdcc49b079d56b0e5b16848b70832ffd1f1574
Steven Nguyen <stevenn38@hotmail.com>
2022-02-08 15:04:39 +1300

    Create file for storing global colors

---
+++ src/styles/colors.js
@@ -0,0 +1,15 @@
+const colors = {
+    main: "#070724",
+    shade: "#38384f",
+    grey: "#838392",
+    mercury: "#419ebb",
+    venus: "#eda249",
+    earth: "#6f2ed6",
+    mars: "#d14c32",
+    jupiter: "#d83a34",
+    saturn: "#cd5120",
+    uranus: "#1ec2a4",
+    neptune: "#2d68f0",
+};
+
+export default colors;
