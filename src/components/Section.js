7d327fec064701266330313c95411e422c332aed
Steven Nguyen <stevenn38@hotmail.com>
2022-02-18 20:08:00 +1300

    Apply laptop media query to section padding

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,7 +1,13 @@
 import styled from "styled-components";
+// Media Query
+import screen from "styles/screen";
 
 const Section = styled.section`
     padding: 4.8rem 12.8rem;
+
+    @media ${screen.laptop} {
+        padding: 7.2rem;
+    }
 `;
 
 export default Section;
