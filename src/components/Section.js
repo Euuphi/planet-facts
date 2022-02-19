dcb7af06cf1e7a93b18209b1b5303949c9d6e5c0
Steven Nguyen <stevenn38@hotmail.com>
2022-02-20 09:24:44 +1300

    Rename screen.js

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 // Media Query
-import screen from "styles/screen";
+import screen from "styles/screens";
 
 const Section = styled.section`
     padding: 4.8rem 12.8rem;
