aa2d9a0ac06904bbc3b6a8cb59fbf12d54c96f1f
Steven Nguyen <stevenn38@hotmail.com>
2022-02-20 09:19:19 +1300

    Write function in screen.js to return all media query strings

--- src/styles/screen.js
+++ src/styles/screen.js
@@ -1,17 +1,28 @@
-const calcScreen = () => {
-    /*
+/*
     --- Media Query Screen Sizes
         Laptop: 1360px - 85em
+        Laptop (small): 1104px - 69em
         Tablet: 768px - 48em
         Mobile: 384px - 24em
 
-    */
-    const sizes = {
-        laptop: "85em",
-    };
+*/
 
+// Object containing all media query sizes
+const sizes = {
+    laptop: "85em", // 1360px
+    laptopS: "69em", // 1104px
+};
+
+/**
+ * Returns an object containing all media query strings from input sizes
+ *
+ * @param {Object} sizes - Object of screen sizes
+ * @return {Object} - Object containing media query string for all input sizes
+ */
+const calcScreen = (sizes) => {
     const device = {};
 
+    // Create and return object with all media  query strings
     for (const screenSize in sizes) {
         device[screenSize] = `(max-width: ${sizes[screenSize]})`;
     }
@@ -19,6 +30,6 @@ const calcScreen = () => {
     return device;
 };
 
-const screen = calcScreen();
+const screen = calcScreen(sizes);
 
 export default screen;
