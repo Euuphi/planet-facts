964938b04e18a864b9a50096b2552027ac18467d
Steven Nguyen <stevenn38@hotmail.com>
2022-03-01 16:33:12 +1300

    Set menu to closed when screen size is above 688px

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,16 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import styled from "styled-components";
 // Themes
 import colors from "styles/colors";
 // Medai Query
 import screen from "styles/screens";
 import useMediaQuery from "@mui/material/useMediaQuery";
+// Context
+import { NavContext } from "context/MenuContext";
+import { PageStateContext } from "context/PageContext";
 // Components
 import DesktopNav from "components/nav/DesktopNav";
 import MobileNav from "components/nav/MobileNav";
-import { PageStateContext } from "context/PageContext";
 
 const HeaderContainer = styled.header`
     border-bottom: 1px solid ${colors.shade};
@@ -47,13 +49,23 @@ const Title = styled.span`
 `;
 
 const Header = () => {
+    // Media query for switching navigation bar
     const tabletS = useMediaQuery(screen.tabletS);
 
+    // Page view context
     const { setView } = useContext(PageStateContext);
+    // Menu context
+    const { setMenuOpen } = useContext(NavContext);
 
+    // Click handler to reset page view
     const navigationClickHandler = () => {
         setView("overview");
     };
+
+    // Close menu when screen size is larger than 688px
+    useEffect(() => {
+        !tabletS && setMenuOpen(false);
+    }, [tabletS, setMenuOpen]);
 
     return (
         <HeaderContainer>
